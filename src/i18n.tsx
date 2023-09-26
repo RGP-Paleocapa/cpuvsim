25d28a12e391a17c2d3aae50fab1e9231d75201a
RGP-Paleocapa <puiu.raresgabriel.studente@itispaleocapa.it>
2023-09-26 02:53:23 +0200

    Add i18n loadPah change

--- src/i18n.tsx
+++ src/i18n.tsx
@@ -13,7 +13,8 @@ i18n
     debug: false,
     saveMissing: false,
     backend: {
-      loadPath: '/locales/{{lng}}/{{ns}}.json',
+      loadPath: 'https://github.com/RGP-Paleocapa/cpuvsim/blob/main/public/locales/{{lng}}/{{ns}}.json',
+      // Use the correct GitHub URL for your JSON files
     },
     ns: ['header', 'footer', 'page1', 'page2'], // Use an array for namespaces
     defaultNS: 'page2', // Set the default namespace
