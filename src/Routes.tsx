fa206f051e6ca1b8fabc13f9407b35d361854564
Puiu Rares Gabriel <puiu.raresgabriel.studente@itispaleocapa.it>
2023-11-18 06:35:27 +0100

    .

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,33 +1,37 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Outlet } from 'react-router-dom';
 import Home from '@/cpuvsim/components/layout/Home';
 import About from '@/cpuvsim/components/layout/About';
 import { NotFound, Maintenance } from '@components/errors/Errors';
 import { Page1, Page2, Page3, Page4, Page5, Page6, Page7, Page8, Page9 } from '@pages/pages';
 
 const AppRoutes = () => {
-    // No need to specify the base path in each route
-    return (
-        <Routes>
-            {/* Static */}
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
+  // Define the base path for your routes
+  const basePath = '/cpuvsim';
 
-            {/* Pages */}
-            <Route path="/page1" element={<Page1 />} />
-            <Route path="/page2" element={<Page2 />} />
-            <Route path="/page3" element={<Page3 />} />
-            <Route path="/page4" element={<Page4 />} />
-            <Route path="/page5" element={<Page5 />} />
-            <Route path="/page6" element={<Page6 />} />
-            <Route path="/page7" element={<Page7 />} />
-            <Route path="/page8" element={<Page8 />} />
-            <Route path="/page9" element={<Page9 />} />
+  return (
+    <Routes>
+      {/* Static */}
+      <Route path={`${basePath}/`} element={<Outlet />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Route>
 
-            {/* Errors */}
-            <Route path="/*" element={<NotFound />} />
-            <Route path="/maintenance" element={<Maintenance />} />
-        </Routes>
-    );
+      {/* Pages */}
+      <Route path={`${basePath}/page1`} element={<Page1 />} />
+      <Route path={`${basePath}/page2`} element={<Page2 />} />
+      <Route path={`${basePath}/page3`} element={<Page3 />} />
+      <Route path={`${basePath}/page4`} element={<Page4 />} />
+      <Route path={`${basePath}/page5`} element={<Page5 />} />
+      <Route path={`${basePath}/page6`} element={<Page6 />} />
+      <Route path={`${basePath}/page7`} element={<Page7 />} />
+      <Route path={`${basePath}/page8`} element={<Page8 />} />
+      <Route path={`${basePath}/page9`} element={<Page9 />} />
+
+      {/* Errors */}
+      <Route path={`${basePath}/*`} element={<NotFound />} />
+      <Route path={`${basePath}/maintenance`} element={<Maintenance />} />
+    </Routes>
+  );
 }
 
 export default AppRoutes;
