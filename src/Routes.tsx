5ae26587f93cfec89b7fa09d6d3788a5f341402b
Puiu Rares Gabriel <puiu.raresgabriel.studente@itispaleocapa.it>
2023-11-18 06:23:35 +0100

    -

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -5,26 +5,27 @@ import { NotFound, Maintenance } from '@components/errors/Errors';
 import { Page1, Page2, Page3, Page4, Page5, Page6, Page7, Page8, Page9 } from '@pages/pages';
 
 const AppRoutes = () => {
+    // No need to specify the base path in each route
     return (
         <Routes>
             {/* Static */}
-            <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
-            <Route path={`${process.env.PUBLIC_URL}/about`} element={<About />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
 
             {/* Pages */}
-            <Route path={`${process.env.PUBLIC_URL}/page1`} element={<Page1 />} />
-            <Route path={`${process.env.PUBLIC_URL}/page2`} element={<Page2 />} />
-            <Route path={`${process.env.PUBLIC_URL}/page3`} element={<Page3 />} />
-            <Route path={`${process.env.PUBLIC_URL}/page4`} element={<Page4 />} />
-            <Route path={`${process.env.PUBLIC_URL}/page5`} element={<Page5 />} />
-            <Route path={`${process.env.PUBLIC_URL}/page6`} element={<Page6 />} />
-            <Route path={`${process.env.PUBLIC_URL}/page7`} element={<Page7 />} />
-            <Route path={`${process.env.PUBLIC_URL}/page8`} element={<Page8 />} />
-            <Route path={`${process.env.PUBLIC_URL}/page9`} element={<Page9 />} />
+            <Route path="/page1" element={<Page1 />} />
+            <Route path="/page2" element={<Page2 />} />
+            <Route path="/page3" element={<Page3 />} />
+            <Route path="/page4" element={<Page4 />} />
+            <Route path="/page5" element={<Page5 />} />
+            <Route path="/page6" element={<Page6 />} />
+            <Route path="/page7" element={<Page7 />} />
+            <Route path="/page8" element={<Page8 />} />
+            <Route path="/page9" element={<Page9 />} />
 
             {/* Errors */}
-            <Route path={`${process.env.PUBLIC_URL}/*`} element={<NotFound />} />
-            <Route path={`${process.env.PUBLIC_URL}/maintenance`} element={<Maintenance />} />
+            <Route path="/*" element={<NotFound />} />
+            <Route path="/maintenance" element={<Maintenance />} />
         </Routes>
     );
 }
