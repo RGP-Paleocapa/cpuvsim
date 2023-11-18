7cbb584b9af2745abafe7cf39dffb2ffcb63d640
Puiu Rares Gabriel <puiu.raresgabriel.studente@itispaleocapa.it>
2023-11-18 06:25:55 +0100

    .

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename='/  cpuvsim'>
+    <BrowserRouter basename='/cpuvsim'>
       <React.Suspense fallback="loading">
         <App />
       </React.Suspense>
