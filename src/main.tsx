2c1d30b6c67a2c1654aefc7d3effe106ca906bda
Puiu Rares Gabriel <puiu.raresgabriel.studente@itispaleocapa.it>
2023-11-18 06:25:28 +0100

    .

--- src/main.tsx
+++ src/main.tsx
@@ -6,19 +6,10 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {process.env.NODE_ENV === 'production' ? (
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
-        <React.Suspense fallback="loading">
-          <App />
-        </React.Suspense>
-      </BrowserRouter>
-    ) : (
-      <BrowserRouter>
-        <React.Suspense fallback="loading">
-          <App />
-        </React.Suspense>
-      </BrowserRouter>
-    )}
+    <BrowserRouter basename='/  cpuvsim'>
+      <React.Suspense fallback="loading">
+        <App />
+      </React.Suspense>
+    </BrowserRouter>
   </React.StrictMode>,
-);
-
+)
