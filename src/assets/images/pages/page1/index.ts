e0ddde1de053a4808834ee84394c209fae0e780b
Puiu Rares Gabriel <puiu.raresgabriel.studente@itispaleocapa.it>
2023-12-05 03:59:16 +0100

    Changed image. Previously updated hamburger icom

--- src/assets/images/pages/page1/index.ts
+++ src/assets/images/pages/page1/index.ts
@@ -1,6 +1,7 @@
-import image1 from './slider/amazon.png';
+// import image1 from './slider/amazon.png';
+import image1 from './slider/controller.png';
 import image2 from './slider/car.png';
-import image3 from './slider/controller.png';
+import image3 from './slider/smartRefrigerator.png';
 import image4 from './slider/drone.png';
 import image5 from './slider/laptop.png';
 import image6 from './slider/smartwatch.png';
@@ -16,9 +17,9 @@ interface ImageObject {
 }
 
 export const images: Record<string, ImageObject> = {
-    image1: { src: image1, alt: 'Robotic vacuum cleaners (aka Roomba)'},
-    image2: { src: image2, alt: 'Modern Car'},
-    image3: { src: image3, alt: 'Controller'},
+    image1: { src: image1, alt: 'Controller'},
+    image2: { src: image2, alt: 'Smart Car'},
+    image3: { src: image3, alt: 'Smart Refrigerator'},
     image4: { src: image4, alt: 'Drone'},
     image5: { src: image5, alt: 'Laptop'},
     image6: { src: image6, alt: 'Smartwatch'},
