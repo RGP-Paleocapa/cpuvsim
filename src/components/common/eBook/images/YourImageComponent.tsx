aab65fc1aed838611049aeab8c477d14f2ed9e84
Puiu Rares Gabriel <puiu.raresgabriel.studente@itispaleocapa.it>
2023-12-07 12:34:34 +0100

    Mouseover effect added

--- src/components/common/eBook/images/YourImageComponent.tsx
+++ src/components/common/eBook/images/YourImageComponent.tsx
@@ -1,3 +1,5 @@
+import React, { useState } from 'react';
+
 // Define a type for the image prop
 type ImageObject = {
   src: string;
@@ -9,9 +11,12 @@ type ImageObject = {
 type YourImageComponentProps = {
   image: ImageObject;
   className?: string;
+  text?: string;
 };
 
-const YourImageComponent: React.FC<YourImageComponentProps> = ({ image, className }) => {
+const YourImageComponent: React.FC<YourImageComponentProps> = ({ image, className, text = 'Click to open link' }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
   // Function to handle click
   const handleClick = () => {
     if (image.url) {
@@ -23,16 +28,28 @@ const YourImageComponent: React.FC<YourImageComponentProps> = ({ image, classNam
   };
 
   // Combine the default classes with any additional classes passed in
-  const combinedClassName = `img-center rounded ${className || ''}`;
+  const baseClassName = 'img-center rounded';
+  const combinedClassName = `${baseClassName} ${className || ''}`;
 
   return (
-    <img
-      src={image.src}
-      alt={image.alt || 'Image description'}
+    <div
+      className={`relative ${isHovered ? 'hover:opacity-80' : ''}`}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
       onClick={image.url ? handleClick : undefined}
-      className={combinedClassName}
-      style={{ cursor: image.url ? 'pointer' : 'default' }}
-    />
+    >
+      <img
+        src={image.src}
+        alt={image.alt || 'Image description'}
+        className={combinedClassName}
+        style={{ cursor: image.url ? 'pointer' : 'default' }}
+      />
+      {text && isHovered && (
+        <div className="absolute inset-0 flex items-center justify-center text-white text-lg font-bold bg-black bg-opacity-80">
+          {text}
+        </div>
+      )}
+    </div>
   );
 };
 
