9c7aa00ad0789f398fb7a5f67c0401dd166a8fd1
Puiu Rares Gabriel <puiu.raresgabriel.studente@itispaleocapa.it>
2023-12-10 18:24:45 +0100

    References h4 -> h2

--- src/components/layout/footer/Authors.tsx
+++ src/components/layout/footer/Authors.tsx
@@ -16,7 +16,7 @@ const Authors = () => {
 
     return (
       <div className="relative flex flex-col items-center md:items-start">
-        <h4 className="font-bold text-lg mb-2">Credits :</h4>
+        <h2 className="font-bold text-lg mb-2">Credits:</h2>
       <button
         onClick={() => setShowAuthors(!showAuthors)}
         className="text-blue-600 dark:text-blue-400 hover:text-blue-600 dark:hover:text-blue-400 transition duration-300"
@@ -26,7 +26,7 @@ const Authors = () => {
       {showAuthors && (
         <div className="fixed inset-0 flex items-center justify-center bg-white dark:bg-gray-800 bg-opacity-80 z-50">
           <div className="bg-white dark:bg-gray-800 p-4 rounded-lg shadow-lg">
-            <h4 className="font-bold text-lg mb-2">Authors:</h4>
+            <h2 className="font-bold text-lg mb-2">Authors:</h2>
             <Ul>
               {credits.map((credit, index) => (
                 <li key={index}>{credit}</li>
