a18e7ef3c77f3779ae19f89b53023b429935ecd8
Puiu Rares Gabriel <puiu.raresgabriel.studente@itispaleocapa.it>
2023-12-10 21:06:34 +0100

    .

--- src/assets/images/pages/page1/index.ts
+++ src/assets/images/pages/page1/index.ts
@@ -1,8 +1,8 @@
 // import image1 from './slider/amazon.png';
-import image1 from './slider/controller.png';
+import image1 from './slider/drone.png';
 import image2 from './slider/car.png';
 import image3 from './slider/smartRefrigerator.png';
-import image4 from './slider/drone.png';
+import image4 from './slider/controller.png';
 import image5 from './slider/laptop.png';
 import image6 from './slider/smartwatch.png';
 import architecture1 from './images/architecture_01.jpg';
@@ -17,10 +17,10 @@ interface ImageObject {
 }
 
 export const images: Record<string, ImageObject> = {
-    image1: { src: image1, alt: 'Controller'},
+    image4: { src: image1, alt: 'Drone'},
     image2: { src: image2, alt: 'Smart Car'},
     image3: { src: image3, alt: 'Smart Refrigerator'},
-    image4: { src: image4, alt: 'Drone'},
+    image1: { src: image4, alt: 'Controller'},
     image5: { src: image5, alt: 'Laptop'},
     image6: { src: image6, alt: 'Smartwatch'},
     image7: { src: modernComputer, alt: 'default7'},
--- src/components/common/SmoothScrollButtonProps.tsx
+++ src/components/common/SmoothScrollButtonProps.tsx
@@ -23,7 +23,7 @@ const SmoothScrollButton: React.FC<SmoothScrollButtonProps> = ({
 
   return (
     <button
-      className="text-sm underline custom-button text-blue-700"
+      className="text-sm underline custom-button text-blue-700 focus:text-blue-900 hover:text-blue-900"
       onClick={() => smoothScrollToId(targetId)}
     >
       {spaceBefore}
