a3c19e1fd74340b94e8fcca6b229c5fc67da2c77
Puiu Rares Gabriel <puiu.raresgabriel.studente@itispaleocapa.it>
2023-12-11 01:11:32 +0100

    .

---
+++ src/components/common/SvgIcons.tsx
@@ -0,0 +1,33 @@
+// Export the HamburgerMenuIcon as a functional component
+export const HamburgerMenuIcon: React.FC = () => {
+  return (
+    <svg
+      className="h-6 w-6 dark:text-gray-200 dark:hover:text-gray-100 dark:focus:text-gray-100"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      role="img"
+      aria-hidden="true"
+    >
+      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
+    </svg>
+  );
+};
+
+// Export the CloseIcon as a functional component
+export const CloseIcon: React.FC = () => {
+  return (
+    <svg
+      className="h-6 w-6 dark:text-gray-200 dark:hover:text-gray-100 dark:focus:text-gray-100"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      role="img"
+      aria-hidden="true"
+    >
+      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
+    </svg>
+  );
+};
--- src/components/common/eBook/EBook.tsx
+++ src/components/common/eBook/EBook.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import NavigationMenu from '@common/eBook/navigation/NavigationMenu';
 import SwitchPage from '@common/eBook/navigation/SwitchPage';
-import HamburgerMenuIcon from '../HamburgerMenuIcon';
+import { HamburgerMenuIcon, CloseIcon } from '../SvgIcons';
 
 interface EBookProps {
   children: React.ReactNode;
@@ -28,7 +28,8 @@ const EBookPage: React.FC<EBookProps> = ({ children, currentPage }) => {
           <div className={`w-full flex-1 ${isNavOpen ? 'overflow-hidden' : ''}`}>
             <div className={`mx-auto ${isNavOpen ? 'max-w-screen-xl px-4' : 'px-4 md:max-w-fit lg:max-w-full'}`}>
               <button onClick={() => setIsNavOpen(!isNavOpen)} className="hidden lg:block open-nav-button">
-                <HamburgerMenuIcon isOpen={isNavOpen} />
+                {!isNavOpen && <HamburgerMenuIcon /> }
+                {isNavOpen && <CloseIcon /> }
               </button>
               <div className={`ebook-page-layout ${isNavOpen ? 'md:ml-[30px] lg:ml-140 2xl:ml-0' : 'md:ml-0 lg:ml-0'}`}>
                 <SwitchPage currentPage={currentPage} />
