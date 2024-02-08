c07d92a8a8a4d564ca2055c796080d3c4d0b6fce
Puiu Rares Gabriel <puiu.raresgabriel.studente@itispaleocapa.it>
2024-02-08 11:14:13 +0100

    About page translated

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,6 +1,7 @@
 import { useFooter } from "@/context/FooterContext";
 import useDocumentMetadata from "@hooks/useDocumentMetadata";
 import { useEffect } from "react";
+import { useTranslation } from "react-i18next";
 
 const About = () => {
   useDocumentMetadata('About - CPU E-Book', 'Learn more about our mission and vision.');
@@ -12,37 +13,22 @@ const About = () => {
     ]);
   }, [setFooterData]);
 
+  const { t } = useTranslation("about");
+
   
   return (
     <div className="container mx-auto py-8 px-4 sm:px-6 lg:px-8">
       <h1 className="text-3xl md:text-4xl lg:text-5xl text-center mb-6 font-bold text-gray-800 dark:text-white">
-        About Us
+        {t("title")}
       </h1>
       <p className="text-lg text-gray-600 dark:text-gray-400 text-center mb-8">
-        Discover the story behind our platform and what drives us.
+        {t("subtitle")}
       </p>
-
-      {/* Image Section */}
-      {/* <div className="flex justify-center mb-8">
-        <img
-          src="/your-about-image.jpg" // Replace with the path to your about image
-          alt="About Us"
-          className="max-w-full h-auto rounded-lg shadow-lg"
-          style={{ maxWidth: '600px' }} // Set a maximum width for the image
-        />
-      </div> */}
 
       {/* Content Section */}
       <div className="text-lg text-gray-600 dark:text-gray-400 space-y-4">
         <p className="text-justify">
-          This e-book, along with the accompanying CPUVSIM software, 
-          has been created by students for students as part of a multi-year OER-enabled pedagogy project integrated into their traditional educational activities. 
-          This approach seeks to address the unfortunate waste of resources associated with "disposable" traditional assignments. 
-          Instead, it demonstrates the feasibility of a self-sustainable process where students actively participate in iteratively improving and extending open resources. 
-          Through this, they develop their professionalism while contributing to the common good. 
-          These activities, conducted within the framework of Computing for Social Good, 
-          aim to prepare the next generation of computer scientists: 
-          not just equipped with coding skills but also empowered to wield technology for the betterment of society.
+          {t("content")}
         </p>
       </div>
     </div>
