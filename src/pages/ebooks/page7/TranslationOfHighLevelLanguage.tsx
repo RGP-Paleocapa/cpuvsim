47537b6c1438cb3305bd50c1589cb5b09605612e
Puiu Rares Gabriel <puiu.raresgabriel.studente@itispaleocapa.it>
2024-02-17 17:15:57 +0100

    Finished page7 translation

--- src/pages/ebooks/page7/TranslationOfHighLevelLanguage.tsx
+++ src/pages/ebooks/page7/TranslationOfHighLevelLanguage.tsx
@@ -4,7 +4,7 @@ import EBookPage from "@/components/features/eBook/EBook";
 import { SectionTitle, Text } from "@/components/features/eBook/content";
 import images from "@assets/images/pages/page7";
 import ImageSwitcher from "@/components/features/eBook/images/ImageAndTextSwitcher";
-import data from './data.json';
+// import data from './data.json';
 import { Section } from "@/components/features/eBook/layout/layout";
 import { useTranslation } from "react-i18next";
 import RenderHTML from "@/components/common/RenderHTML";
@@ -17,14 +17,22 @@ const Page7 = () => {
       setFooterData([]);
     }, [setFooterData]);
 
-    const sumListText = data.sum;
-    const formulaListText = data.formula;
-    const infiniteLoopListText = data.infiniteLoop;
-    const ifThenElseListText = data.ifThenElse;
-    const whileListText = data.while;
-    const forLoopListText = data.forLoop;
+    // const sumListText = data.sum;
+    // const formulaListText = data.formula;
+    // const infiniteLoopListText = data.infiniteLoop;
+    // const ifThenElseListText = data.ifThenElse;
+    // const whileListText = data.while;
+    // const forLoopListText = data.forLoop;
 
     const { t } = useTranslation("page7");
+
+    // Fetch each translation, ensuring it's returned as an object (array of arrays)
+    const sumListText: string[][] = t("sum", { returnObjects: true });
+    const formulaListText: string[][] = t("formula", { returnObjects: true });
+    const infiniteLoopListText: string[][] = t("infiniteLoop", { returnObjects: true });
+    const ifThenElseListText: string[][] = t("ifThenElse", { returnObjects: true });
+    const whileListText: string[][] = t("while", { returnObjects: true });
+    const forLoopListText: string[][] = t("forLoop", { returnObjects: true });
 
     return (
         <EBookPage currentPage={7}>
