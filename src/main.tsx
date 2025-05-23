d2142fe1e22d12d589c7d016dddfa27ac5ed3316
Puiu Rares Gabriel <puiu.raresgabriel.studente@itispaleocapa.it>
2025-05-23 18:49:58 +0200

    refactor: add ErrorBoundary to main

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,26 @@ import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import App from './App';
 import Loading from '@components/utils/Loading';
+import { ErrorBoundary } from 'react-error-boundary';
 
 import './index.css';
 import './trusted-security-policies';
+import ErrorFallback from './components/utils/ErrorFallback';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HashRouter>
-      <React.Suspense fallback={<Loading />}>
-        <App />
-      </React.Suspense>
+        <ErrorBoundary
+          FallbackComponent={ErrorFallback}
+          onReset={() => {
+            // Reset the state or refresh
+            window.location.reload();
+          }}
+        >
+          <React.Suspense fallback={<Loading />} >
+            <App />
+          </React.Suspense>
+        </ErrorBoundary>
     </HashRouter>
   </React.StrictMode>
 );
