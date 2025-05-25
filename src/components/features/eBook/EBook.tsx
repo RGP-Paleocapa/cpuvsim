ed2521a0819dfaf728eb5bf59f73e6cd2e9cd344
Puiu Rares Gabriel <puiu.raresgabriel.studente@itispaleocapa.it>
2025-05-25 12:08:51 +0200

    refactor(EBook) use semantic HTML instead of divs

--- src/components/features/eBook/EBook.tsx
+++ src/components/features/eBook/EBook.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import NavigationMenu from '@/components/features/eBook/navigation/NavigationMenu';
 import SwitchPage from '@/components/features/eBook/navigation/SwitchPage';
 import { HamburgerMenuIcon, CloseIcon } from '@common/SvgIcons';
+import { NavigationMenu } from '@components/features/eBook/navigation/EBookNavigation';
 
 interface EBookProps {
   children: React.ReactNode;
@@ -21,30 +21,45 @@ const EBookPage: React.FC<EBookProps> = ({ children, currentPage }) => {
   }, [isNavOpen]);
 
   return (
-    <div className={`bg-slate-100 dark:bg-slate-900 min-h-screen ${isNavOpen ? 'overflow-hidden' : ''}`}>
+<main className={`bg-slate-100 dark:bg-slate-900 min-h-screen ${isNavOpen ? 'overflow-hidden' : ''}`}>
       <div className="container mx-auto py-4 sm:px-6 md:px-0">
         <div className="flex flex-col md:flex-row">
-          <div className={`w-full md:w-0 lg:w-80 ${isNavOpen ? '' : 'hidden'}`}>
-            <NavigationMenu currentPage={currentPage} totalPages={8} isNavOpen={isNavOpen} setIsNavOpen={setIsNavOpen} />
-          </div>
-          <div className={`w-full flex-1 ${isNavOpen ? 'overflow-hidden' : ''}`}>
+
+          {/* Sidebar Navigation */}
+          <aside className={`w-full md:w-0 lg:w-80 ${isNavOpen ? '' : 'hidden'}`}>
+            <NavigationMenu
+              currentPage={currentPage}
+              totalPages={8}
+              isNavOpen={isNavOpen}
+              setIsNavOpen={setIsNavOpen}
+            />
+          </aside>
+
+          {/* Main Content Area */}
+          <section className={`w-full flex-1 ${isNavOpen ? 'overflow-hidden' : ''}`}>
             <div className={`mx-auto ${isNavOpen ? 'max-w-screen-xl px-4' : 'px-4 md:max-w-fit lg:max-w-full'}`}>
-              <button onClick={() => setIsNavOpen(!isNavOpen)} className="hidden lg:block open-nav-button">
-                {/* Render the HamburgerMenuIcon when navigation is closed */}
+
+              {/* Toggle Navigation */}
+              <button
+                onClick={() => setIsNavOpen(!isNavOpen)}
+                className="hidden lg:block open-nav-button"
+                aria-label={isNavOpen ? 'Close navigation' : 'Open navigation'}
+              >
                 {!isNavOpen && <HamburgerMenuIcon />}
-                {/* Render the CloseIcon when navigation is open */}
                 {isNavOpen && <CloseIcon />}
               </button>
-              <div className={`ebook-page-layout ${isNavOpen ? 'md:ml-[30px] lg:ml-140 2xl:ml-140' : 'md:ml-0 lg:ml-0'}`}>
+
+              {/* Page Content */}
+              <article className={`ebook-page-layout ${isNavOpen ? 'md:ml-[30px] lg:ml-140 2xl:ml-140' : 'md:ml-0 lg:ml-0'}`}>
                 <SwitchPage currentPage={currentPage} />
                 {children}
                 <SwitchPage currentPage={currentPage} />
-              </div>
+              </article>
             </div>
-          </div>
+          </section>
         </div>
       </div>
-    </div>
+    </main>
   );
 };
 
