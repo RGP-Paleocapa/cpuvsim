b79fb0a95162661dbd4d8b52d479b8b0bb2f91da
Puiu Rares Gabriel <puiu.raresgabriel.studente@itispaleocapa.it>
2025-05-25 21:16:35 +0200

    fix(ListItem): add noParagraph option to suppor Pre tags

--- src/components/features/eBook/typography/ListItem.tsx
+++ src/components/features/eBook/typography/ListItem.tsx
@@ -5,15 +5,17 @@ const ListItem = ({
   children,
   className,
   margin0,
+  noParagraph,
   ...restProps
 }: {
   children: ReactNode;
   className?: string;
   isDark?: boolean;
   margin0?: boolean;
+  noParagraph?: boolean
 }) => (
   <li className={`${margin0 ? "mb-0" : "mb-4"} ${className}`}>
-    <ParagraphText {...restProps}>{children}</ParagraphText>
+    {noParagraph ? children : <ParagraphText {...restProps}>{children}</ParagraphText>}
   </li>
 );
 
